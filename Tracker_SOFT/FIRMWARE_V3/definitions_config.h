d4dcf1fa94415c854801f6d2ccd3b88f712fe268
radio-satellites <VE3SVF@gmail.com>
2024-03-11 11:28:19 -0400

    Add more

--- Tracker_SOFT/FIRMWARE_V3/definitions_config.h
+++ Tracker_SOFT/FIRMWARE_V3/definitions_config.h
@@ -1,6 +1,6 @@
 ////////////////////////CHANGE THESE////////////////////////
 //Change these for a regular flight. These are the "basic" settings
-static const char callsign[] = "OS42-1"; // maximum of 6 characters. "T" should be used for testing, "X" for unknown and any other letter of the alphabet to differentiate between flights
+static const char callsign[] = "OS42-2"; // maximum of 6 characters. "T" should be used for testing, "X" for unknown and any other letter of the alphabet to differentiate between flights
 #define TEMP_CALIB 2 //Calibration factor for temperature sensor. Calibrate your own sensor. See calibration.txt.
 #define GPS_INTERLEAVE 3 //Send a GPS packet every X imagery packets
 #define IMAGERY_START_ITERATIONS  5 //After X initial GPS telemetry cycles, start sending imagery
@@ -21,7 +21,7 @@ static const char callsign[] = "OS42-1"; // maximum of 6 characters. "T" should 
 
 ////////////////////////ADVANCED SETTINGS////////////////////////
 #define USE_WDT true //Enable/disable watchdog for testing
-#define DEBUG_MSG false //Allow debug messages through the Serial port
+#define DEBUG_MSG true //Allow debug messages through the Serial port
 #define ALLOW_SLEEP false //Do we restart between images? Usually it's not a good idea, unless we have memory issues
 #define CAM_ERROR_DO "TXGPS+RESTART" //What do we do when there is a camera error? Options: RESTART,TXGPS+RESTART,TXGPS
 #define SET_FLIGHT_MODE_YES true //Turn on flight mode?
@@ -36,10 +36,10 @@ static const char callsign[] = "OS42-1"; // maximum of 6 characters. "T" should 
 /////////////////////////////////////////////////////////////////
 
 //Do not change
-#define NSS_LORATX 12                                  //select on LoRa device
-#define NRESET_LORATX 15                               //R
-#define SCK_LORATX 4                                   //SCK on SPI3
-#define MISO_LORATX 13                                 //MISO on SPI3 
+#define NSS_LORATX 12                                  //select on LoRa device +
+#define NRESET_LORATX 15                               //Reset +
+#define SCK_LORATX 4                                   //SCK on SPI3 + 
+#define MISO_LORATX 13                                 //MISO on SPI3 + 
 #define MOSI_LORATX 2                                  //MOSI on SPI3
 
 #define uS_TO_S_FACTOR 1000000 //Change this if you want to change TIME ITSELF
